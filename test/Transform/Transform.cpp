3c3ca5715677989e0b92da081a9bc973d54bb427
jelly27th <872908391@qq.com>
2023-09-28 21:54:19 +0800

    transform practise 1

--- test/Transform/Transform.cpp
+++ test/Transform/Transform.cpp
@@ -185,8 +185,8 @@ int main()
 
         // create transformations
         glm::mat4 transform = glm::mat4(1.0f); // initialize matrix
-        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
         transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
 
         // set the texture mix value in the shader
         ourShader.setFloat("mixValue", mixValue);
