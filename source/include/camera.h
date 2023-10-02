6993df671a1c64b68d11afbf20f8333a301e10be
jelly27th <872908391@qq.com>
2023-10-02 21:06:32 +0800

    inntroduction end

--- source/include/camera.h
+++ source/include/camera.h
@@ -74,6 +74,8 @@ public:
             Position -= Right * velocity;
         if (direction == RIGHT)
             Position += Right * velocity;
+        
+        Position.y = 0.0f;
     }
 
     // processes input received from a mouse input system. 
